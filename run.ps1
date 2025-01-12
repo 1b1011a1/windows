7cd355da7924642dab030359fb4f1d061b979ef8
1b1011a1 <m13786942808@163.com>
2025-01-12 13:58:17 +0800

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,10 +1,9 @@
-Get-ChildItem -Path ($env:USERPROFILE + "\Desktop" ) | Remove-Item -Recurse -Force
+Get-ChildItem -Path ($env:USERPROFILE + "\Desktop") | Remove-Item -Recurse -Force
 Get-ChildItem -Path "C:\Users\Public\Desktop" | Remove-Item -Recurse -Force
 rm C:\A.msi
 New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons" -Force
 New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Force
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Type DWORD -Value 0
-Stop-Process -Name "explorer" -Force
 Add-WindowsCapability -Online -Name Language.zh-CN~Offline
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\MUI\UILanguages" -Name "Default" -Value zh-CN
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\MUI\UILanguages" -Name "InstallLanguage" -Value zh-CN
