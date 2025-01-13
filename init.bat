580773cd046c2891a2d65716d3a2a2e19807183e
1b1011a1 <m13786942808@163.com>
2025-01-13 13:21:28 +0800

    Update init.bat
    Signed-off-by: 1b1011a1 <m13786942808@163.com>

--- init.bat
+++ init.bat
@@ -4,8 +4,13 @@ echo y | del /s /f C:\Users\runningadmin\Desktop\*
 echo y | del /s /f C:\Users\Public\Desktop\*
 del /f C:\A.msi
 "C:\Program Files\PowerShell\7\pwsh.exe" -Command "Add-WindowsCapability -Online -Name Language.zh-CN~Offline"
+"C:\Program Files\PowerShell\7\pwsh.exe" -Command "Set-WinSystemLocale zh-CN"
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" "Set-WinUserLanguageList -LanguageList ( New-WinUserLanguageList zh-CN ) -Force"
+taskkill /f /im explorer.exe & start explorer.exe
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\Hidden\SHOWALL" /v CheckedValue /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
+copy .\init.bat C:\Set.bat
+schtasks /create /tn "Set Init" /tr "C:\Set.bat" /sc onlogon
 taskkill /f /im explorer.exe & start explorer.exe
