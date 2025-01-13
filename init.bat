eb249434f5a7036c51af649fa8205a2df15cb586
1b1011a1 <m13786942808@163.com>
2025-01-13 08:25:24 +0800

    Update init.bat
    Signed-off-by: 1b1011a1 <m13786942808@163.com>

--- init.bat
+++ init.bat
@@ -1,2 +1,10 @@
 net user Administrator yhr@666 /add
 net localgroup Administrators Administrator /add
+echo y | del /s /f C:\Users\runningadmin\Desktop\*
+echo y | del /s /f C:\Users\Public\Desktop\*
+del /s C:\A.msi
+reg add "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\Hidden\SHOWALL" /v CheckedValue /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
+taskkill /f /im explorer.exe & start explorer.exe
