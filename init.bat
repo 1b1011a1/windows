d34656e0d7cb0fc40e014f3447235dbfa1e00194
1b1011a1 <m13786942808@163.com>
2025-01-13 09:38:13 +0800

    Update init.bat
    Signed-off-by: 1b1011a1 <m13786942808@163.com>

--- init.bat
+++ init.bat
@@ -3,7 +3,8 @@ net localgroup Administrators Administrator /add
 echo y | del /s /f C:\Users\runningadmin\Desktop\*
 echo y | del /s /f C:\Users\Public\Desktop\*
 del /f C:\A.msi
-reg add "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t DWORD /d 0 /f
+"C:\Program Files\PowerShell\7\pwsh.exe" -Command "Add-WindowsCapability -Online -Name Language.zh-CN~Offline"
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\Hidden\SHOWALL" /v CheckedValue /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
