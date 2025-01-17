20048fd6c1aff4609f5a0378db67c0791aec5222
1b1011a1 <m0_54218178@noreply.gitcode.com>
2025-01-17 20:28:57 +0800

    test

---
+++ WallPaper/1.py
@@ -0,0 +1,3 @@
+import ctypes
+import os
+ctypes.windll.user32.SystemParametersInfoW(20, 0, r"C:\Windows\Web\Wallpaper\Windows\WallPaper.jpg", 0)
--- init.bat
+++ init.bat
@@ -10,37 +10,18 @@ if "%1"=="" (
   copy .\init.bat C:\Set.bat
   copy .\zh-cn.zip C:\zh-cn.zip
   copy .\zh-cn.ps1 C:\zh-cn.ps1
-  REM 禁用了，bug一堆。
-  REM copy .\zh-cn.py C:\zh-cn.py
-  REM copy .\1.PNG C:\1.png
-  REM copy .\2.PNG C:\2.png
-  REM copy .\3.PNG C:\3.png
-  REM copy .\4.PNG C:\4.png
-  REM copy .\5.PNG C:\5.png
-  REM copy .\6.PNG C:\6.png
-  REM copy .\7.PNG C:\7.png
+  copy .\WallPaper\1.py C:\1.py
+  copy .\WallPaper\2.jpg C:\Windows\Web\Wallpaper\Windows\WallPaper.jpg
   Reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 0 /f
+  pip install ctypes
   "C:\Program Files\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -File "C:\zh-cn.ps1"
   "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Command "Set-WinSystemLocale zh-CN"
   echo | runas /user:Administrator "C:\Set.bat /set"
   net user Administrator yhr@666
-  REM 禁用了，bug一堆。
-  REM reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v SetSystem /t REG_SZ /d "C:\Set.bat /init" /f
-  REM pip install opencv-python
-  REM pip install numpy
-  REM pip install pillow
-  REM pip install pywinauto
-  REM pip install pyautogui
   del /f /q C:\Set.bat
   del /f /q C:\zh-cn.zip
   del /f /q C:\zh-cn.ps1
   del /f /q /s C:\zh-cn\*
-) else if "%1"=="/init" (
-  REM 禁用了，bug一堆。
-  REM reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v SetSystem /f
-  REM echo 仅用于自动完成一些修改，不需要可以直接关掉。
-  REM timeout /t 10 /nobreak
-  REM python C:\zh-cn.py
 ) else if "%1"=="/set" (
   reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
   reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
@@ -49,4 +30,5 @@ if "%1"=="" (
   reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton /t REG_DWORD /d 0 /f
   "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Command "Set-WinUserLanguageList -LanguageList ( New-WinUserLanguageList zh-CN ) -Force"
   "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Command "Set-WinUILanguageOverride -Language zh-CN"
+  python C:\1.py
 )
