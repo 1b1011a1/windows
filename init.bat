0ce6643699055776339846b108096d2953217745
1b1011a1 <m13786942808@163.com>
2025-07-19 13:54:08 +0800

    Update init.bat
    Signed-off-by: 1b1011a1 <m13786942808@163.com>

--- init.bat
+++ init.bat
@@ -8,13 +8,8 @@ if "%1"=="" (
   del /q /f C:\A.zip
   reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\Hidden\SHOWALL" /v CheckedValue /t REG_DWORD /d 1 /f
   copy .\init.bat C:\Set.bat
-  copy .\zh-cn.zip C:\zh-cn.zip
-  copy .\zh-cn.ps1 C:\zh-cn.ps1
-  copy .\WallPaper\1.py C:\1.py
   copy .\WallPaper\2.jpg C:\Windows\Web\Wallpaper\Windows\WallPaper.jpg
   Reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 0 /f
-  pip install ctypes
-  "C:\Program Files\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -File "C:\zh-cn.ps1"
   "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Command "Set-WinSystemLocale zh-CN"
   echo | runas /user:Administrator "C:\Set.bat /set"
   net user Administrator yhr@666
@@ -24,7 +19,4 @@ if "%1"=="" (
   reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
   reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 0 /f
   reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton /t REG_DWORD /d 0 /f
-  "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Command "Set-WinUserLanguageList -LanguageList ( New-WinUserLanguageList zh-CN ) -Force"
-  "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Command "Set-WinUILanguageOverride -Language zh-CN"
-  python C:\1.py
 )
